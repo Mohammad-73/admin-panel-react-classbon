8e89fb517d27d7416fb08d939f6aeb9c05d351d2
Mohammad-73 <m.khalili7310@gmail.com>
2025-03-27 15:25:52 +0330

    Change theme with context

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { AppProvider } from "./context/app/app-context.jsx";
+import { AppProvider } from "./contexts/app/app-context.jsx";
 
 createRoot(document.getElementById("root")).render(
   <AppProvider>
