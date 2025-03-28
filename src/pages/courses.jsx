8ee9cc5073a701644aaa04346521ab8553999606
Mohammad-73 <m.khalili7310@gmail.com>
2025-03-28 15:43:46 +0330

    Implementation Course list page with dynamic courses data

--- src/pages/courses.jsx
+++ src/pages/courses.jsx
@@ -1,5 +1,25 @@
+import CourseList from "../features/courses/components/course-list";
+import { Await, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+
 const Courses = () => {
-  return <h1>Courses</h1>;
+  const data = useLoaderData();
+  return (
+    <div className="row">
+      <div className="col-12">
+        <div className="d-flex align-items-center justify-content-between mb-5">
+          <a className="btn btn-primary fw-bolder mt-n1">افزون دوره جدید</a>
+        </div>
+        <Suspense
+          fallback={<p className="text-info">در حال دریافت اطلاعات...</p>}
+        >
+          <Await resolve={data.courses}>
+            {(loadedCourses) => <CourseList courses={loadedCourses} />}
+          </Await>
+        </Suspense>
+      </div>
+    </div>
+  );
 };
 
 export default Courses;
