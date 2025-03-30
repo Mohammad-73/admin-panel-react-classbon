c926085827782f462a7b7719d4026ba2bcab1dbe
Mohammad-73 <m.khalili7310@gmail.com>
2025-03-30 10:57:57 +0330

    Implementation modal with react portal feature

--- src/features/categories/components/category-list.jsx
+++ src/features/categories/components/category-list.jsx
@@ -46,7 +46,9 @@ const CategoryList = ({
                             <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
                           </svg>
                         </a>
-                        {/* <a onClick={() => deleteCategory(category.id)}>
+                        <a
+                        // onClick={() => deleteCategory(category.id)}
+                        >
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="24"
@@ -60,7 +62,7 @@ const CategoryList = ({
                             <polyline points="3 6 5 6 21 6"></polyline>
                             <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
                           </svg>
-                        </a> */}
+                        </a>
                       </td>
                     </tr>
                   );
